c911cf6e5ff9a93ef70a94790a1a2409a1c504e1
gaurishukla60 <75981006+gaurishukla60@users.noreply.github.com>
2021-05-13 16:33:53 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
-function setup()
+song1="";
+song2="";
+song1_status="";
+song2_status="";
+function preload()
 {
-    canvas=createCanvas(500,500);
-    canvas.center();
+    song1=loadSound("music.mp3");
+    song2=loadSound("music2.mp3");
+
+}
+function setup()
+    {
+      canvas=createCanvas(600,500);
+      canvas.center();
+      video=createCapture(VIDEO);
+      video.hide();
+    }
+function draw()
+{
+    image(video,0,0,600,500);
 }
