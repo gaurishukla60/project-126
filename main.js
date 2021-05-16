490686e89a6da4819e93c4b68f2adb19002cb04a
gaurishukla60 <75981006+gaurishukla60@users.noreply.github.com>
2021-05-16 16:37:20 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 song1="";
 song2="";
+rightWristX = 0;
+rightWristY = 0;
+leftWristX = 0;
+leftWristY = 0;
+
 song1_status="";
 song2_status="";
 function preload()
@@ -14,8 +19,34 @@ function setup()
       canvas.center();
       video=createCapture(VIDEO);
       video.hide();
-    }
+      poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+ }
+ function modelLoaded() 
+{ 
+    console.log('PoseNet Is Initialized'); 
+}
+
+function gotPoses()
+{
+rightWristX = results[0].pose.rightWrist.x;
+ rightWristY = results[0].pose.rightWrist.y;
+  console.log("rightWristX = " + rightWristX +" rightWristY = "+ rightWristY);
+  
+  leftWristX = results[0].pose.leftWrist.x;
+   leftWristY = results[0].pose.leftWrist.y;
+    console.log("leftWristX = " + leftWristX +" leftWristY = "+ leftWristY);
+}
+
 function draw()
 {
     image(video,0,0,600,500);
 }
+
+function play() 
+{ 
+    song.play();
+     song.setVolume(1);
+      song.rate(1); 
+    }
+
